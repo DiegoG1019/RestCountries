336b3b77c8f11bc644709272926d66a16d015098
Diego García <dagarciam1014@gmail.com>
2021-09-10 17:44:32 -0400

    Began work in enum additions

---
+++ RestCountries/Regions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RestCountries
+{
+    [Serializable, JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Region
+    {
+        [EnumMember(Value = "Europe")] //Name as it appears in the JSON file
+        Europe, //C# Friendly name
+
+        [EnumMember(Value = "Asia")]
+        Asia,
+
+        [EnumMember(Value = "Africa")]
+        Africa,
+
+        [EnumMember(Value = "Oceania")]
+        Oceania,
+
+        [EnumMember(Value = "Americas")]
+        Americas,
+    }
+}
---
+++ RestCountries/SubRegions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RestCountries
+{
+    [Serializable, JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum SubRegion
+    {
+        [EnumMember(Value = "Northern Europe")]
+        NorthEurope,
+
+        [EnumMember(Value = "Southern Europe")]
+        SouthEurope,
+
+        [EnumMember(Value = "Southern Asia")]
+        SouthAsia,
+
+        [EnumMember(Value = "Northern Africa")]
+        NorthAfrica,
+
+        [EnumMember(Value = "Middle Africa")]
+        MiddleAfrica,
+
+        [EnumMember(Value = "Polynesia")]
+        Polynesia,
+
+        [EnumMember(Value = "Caribbean")]
+        Caribbean
+    }
+}
